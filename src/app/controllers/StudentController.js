34417ea7c87e73df79bc8a9f5c268d9dc3bf3d84
antonylrds <antonylrds@gmail.com>
2019-10-20 19:14:54 -0300

    Desafio 02 completo

---
+++ src/app/controllers/StudentController.js
@@ -0,0 +1,74 @@
+import * as Yup from 'yup';
+import Student from '../models/Student';
+
+class StudentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      idade: Yup.number()
+        .integer()
+        .min(3)
+        .required(),
+      peso: Yup.number().required(),
+      altura: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fail' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const student = await Student.create(req.body);
+
+    return res.json(student);
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      id: Yup.number()
+        .integer()
+        .required(),
+      nome: Yup.string(),
+      email: Yup.string().email(),
+      idade: Yup.number()
+        .integer()
+        .min(3),
+      peso: Yup.number(),
+      altura: Yup.number(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fail' });
+    }
+
+    const studentExists = await Student.findOne({
+      where: { email: req.body.email },
+    });
+
+    if (studentExists) {
+      return res.status(400).json({ error: 'Student already exists' });
+    }
+
+    const student = await Student.findByPk(req.body.id);
+
+    if (!student) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    await student.update(req.body);
+
+    return res.json(student);
+  }
+}
+
+export default new StudentController();
